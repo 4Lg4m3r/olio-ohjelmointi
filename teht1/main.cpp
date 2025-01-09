75053d65cdf1d4ef5076e0a649998ce1510a5aa6
Aden <tervonenanni@gmail.com>
2025-01-09 13:56:48 +0200

    Viikkotehtävä 1 valmis

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,56 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
+int game(int maxnum);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int arvaus_lkm = 0;
+    int maxnum = 40;
+    arvaus_lkm = game(maxnum);
+
+    cout << "Arvauksia kaikkiaan: "
+         << arvaus_lkm
+         << endl;
+
     return 0;
+}
+
+
+int game(int maxnum) {
+    srand(time(NULL));
+
+    int satunnaisluku = rand() % maxnum;
+    int arvaus;
+    int arvaus_lkm;
+
+    cout << "Maxnum = "
+         << maxnum
+         << endl;
+
+    while (arvaus != satunnaisluku) {
+        cout << "Anna luku: "
+             << endl;
+        cin >> arvaus;
+        arvaus_lkm++;
+
+        if (arvaus < satunnaisluku) {
+            cout << "Luku on suurempi"
+                 << endl;
+        }
+
+        else if (arvaus > satunnaisluku) {
+            cout << "Luku on pienempi"
+                 << endl;
+        }
+    }
+
+    if (arvaus == satunnaisluku) {
+        cout << "Oikea luku!"
+             << endl;
+    }
+
+    return arvaus_lkm;
 }
