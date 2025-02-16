4d6f36c48be15d3207885567808af7dde5121906
Aden <tervonenanni@gmail.com>
2025-02-16 15:59:54 +0200

    Viikkotehtävä 6 valmis

---
+++ teht6/student.cpp
@@ -0,0 +1,23 @@
+#include "student.h"
+
+Student::Student(string name, int age) : Name(name), Age(age) {}
+
+void Student::setAge(int age) {
+    Age = age;
+}
+
+void Student::setName(string name) {
+    Name = name;
+}
+
+string Student::getName() const {
+    return Name;
+}
+
+int Student::getAge() const {
+    return Age;
+}
+
+void Student::printStudentInfo() const {
+    cout << "Student " << Name << " Age " << Age << endl;
+}
